f6c99c18ce70dc119d63b86731712e1aeced55f0
Aleksandr Ataev <onlyaa94@mail.ru>
2024-04-24 23:37:12 +0300

    Initial Commit

---
+++ FXguide&quiz/FXguide_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FXguide_quizApp.swift
+//  FXguide&quiz
+//
+//  Created by Aleksandr Ataev on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FXguide_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
